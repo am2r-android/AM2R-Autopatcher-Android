af6680f2080f30caf84ceb4c355b140f3aaad818
am2r-android <am2r-android@proton.me>
2026-07-17 17:29:36 -0400

    Delete app-android directory

--- app-android/app/src/main/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE    := xd3jni
-LOCAL_SRC_FILES := xd3jni.c
-LOCAL_CFLAGS    := -O2 -DNDEBUG
-LOCAL_LDLIBS    := -llog
-include $(BUILD_SHARED_LIBRARY)
